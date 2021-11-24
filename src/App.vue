b31cfbece842e8a1d1e3d7021eb5c681bbf56e9d
Salvador Briones <mexidense@gmail.com>
2021-11-24 20:45:13 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <Todos/>
+  </div>
+</template>
+
+<script>
+import Todos from './components/Todos'
+
+export default {
+  name: 'App',
+  components: {
+    Todos
+  }
+}
+</script>
+
+<style>
+#app {
+}
+</style>
