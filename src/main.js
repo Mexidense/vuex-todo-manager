f2b77fb7e1fb3be7c8ac60200f32b88fb71cd84e
Salvador Briones <mexidense@gmail.com>
2022-01-14 20:09:20 +0100

    feat: add Font awesome package

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import store from './store'
 
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
