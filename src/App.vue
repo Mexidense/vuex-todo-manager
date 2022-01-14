21b1722038bd8dd9ad642eb3ea19c47cdeee514f
Salvador Briones <mexidense@gmail.com>
2022-01-14 20:00:24 +0100

    feat: add a new todo

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <div class="container">
+      <AddTodo/>
       <Todos/>
     </div>
   </div>
 </template>
 
 <script>
+import AddTodo from './components/AddTodo.vue'
 import Todos from './components/Todos'
 
 export default {
   name: 'App',
   components: {
+    AddTodo,
     Todos
   }
 }
--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -9,13 +9,19 @@ const getters = {
 }
 
 const mutations = {
-  setTodos: (state, payload) => (state.todos = payload)
+  setTodos: (state, payload) => (state.todos = payload),
+  addTodo: (state, payload) => state.todos.unshift(payload)
 }
 
 const actions = {
   async fetchTodos ({ commit }) {
     const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
     commit('setTodos', response.data)
+  },
+
+  async addTodo ({ commit }, payload) {
+    const response = await axios.post('https://jsonplaceholder.typicode.com/posts', { title: payload, completed: false })
+    commit('addTodo', response.data)
   }
 }
 
